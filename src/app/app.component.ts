24ac1ee6106e8e28f6b74b40535586f5b1ddbf77
Raphael Alvarenga <raphaelalvarenga2@gmail.com>
2022-05-24 14:04:11 -0300

    Pagination funcionality initiated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,8 @@ export class AppComponent implements OnInit {
     title = "challenge-moviik";
 
     quotes$: Observable<Quote[]> = of([]);
-    filteredQuotes: Quote[] = [];
+    filterRange = [0, 10];
+    isPreviousButtonDisabled = false;
 
     constructor(private apiService: ApiService, private store: Store<{ quoteReducer: Quote[] }>) {
         this.quotes$ = this.store.select("quoteReducer");
@@ -30,6 +31,31 @@ export class AppComponent implements OnInit {
     }
 
     filterQuotes() {
-        return this.quotes$.pipe(map(quotes => quotes.filter((quote, index) => index < 3)));
+        return this.quotes$.pipe(
+            map(quotes =>
+                quotes.filter((quote, index) => {
+                    return index > this.filterRange[0] && index <= this.filterRange[1];
+                })
+            )
+        );
+    }
+
+    previousPage() {
+        const [first, last] = this.filterRange;
+
+        if (first >= 0) {
+            this.filterRange = [first - 10, last - 10];
+            this.filterQuotes();
+        } else {
+            this.isPreviousButtonDisabled = true;
+        }
+    }
+
+    nextPage() {
+        const [first, last] = this.filterRange;
+
+        this.filterRange = [first + 10, last + 10];
+        this.filterQuotes();
+        this.isPreviousButtonDisabled = false;
     }
 }
